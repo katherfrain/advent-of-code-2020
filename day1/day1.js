680cbdd0ca529209f79e5c6fe1f25953b1c68812
Katherine Frain <katherfrain@gmail.com>
2020-12-05 15:13:19 -0500

    worked it out

--- day1/day1.js
+++ day1/day1.js
@@ -1,4 +1,5 @@
-//theoretically this should 
+const fs = require('fs')
+
 function setYear(){
     let thisDate = new Date; 
     let thisYear = thisDate.getFullYear(); 
@@ -7,21 +8,23 @@ function setYear(){
 setYear()
 
 //yes, it may be an overly verbose function name, but I prefer to know exactly what my functions do
-function checkIf2InputsSumToYear(fileinput){
-    fetch(fileinput)
-    .then(response => response.json())
-    .then(data => {
-        for(let x=0; x<data.length; x++){
-            let pointA = data[x]
-            for(let j=0; j<data.length; j++) {
-                let pointB = data[j]
-                if(pointA + pointB == setYear()) {
-                    return pointA*pointB
-                }
+ function checkIf2InputsSumToYear(fileinput) {
+
+    var rawFileInput = fs.readFileSync(fileinput, 'utf8');
+    var preppedFileList = rawFileInput.split('\n');
+
+    for (let x = 0; x < preppedFileList.length; x++) {
+       
+        for (let j = 0; j < preppedFileList.length; j++) {
+       
+            let pointA = parseInt(preppedFileList[x])
+            let pointB = parseInt(preppedFileList[j])
+   
+       
+            if (pointA + pointB == setYear()) {
+                return pointA * pointB
             }
         }
-    })
-
+    }
 }
-
-console.log(checkIf2InputsSumToYear('/input'))
+console.log(checkIf2InputsSumToYear('./input'))
