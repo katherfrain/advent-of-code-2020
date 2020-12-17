fadd1f309d871d04cf2fc4e9663c2e2c8267a57d
Katherine Frain <katherfrain@gmail.com>
2020-12-17 17:59:19 -0500

    doing something wrong

--- day4/day4.js
+++ day4/day4.js
@@ -3,31 +3,100 @@ const fs = require('fs')
 function prepareFile(file) {
     let loadedFile = fs.readFileSync(file, 'utf8');
     let preppedFile = loadedFile.split(/\n{2,}/g);
-        //with regex do curly brackets to indicate number of chars to count for 
-        //add the g to look in the global file [reason unclear]
+    //with regex do curly brackets to indicate number of chars to count for 
+    //add the g to look in the global file [reason unclear]
 
     let fullyPreppedFile = preppedFile.map(passport => {
         return passport.split(/\s+/g)
     })
-    return fullyPreppedFile; 
+    return fullyPreppedFile;
 }
 
 
 function validPassports(file) {
-    let arrayOfPassPortArrays = prepareFile(file);
-    console.log(arrayOfPassPortArrays)
-    for(let x = 0; x < arrayOfPassPortArrays.length; x++) {
-        
-        // console.log(arrayOfPassportArrays[x])
-        // let splitOnColon = arrayOfPassPortArrays[x].split(':');
-        // for(let y = 0; y < splitOnColon.length; y++) {
-        // if(splitOnColon[0].includes(/(byr)/) 
-        // && arrayOfPassPortArrays[x].includes('iyr')) {
-        //     console.log('dude')
-        // }
+    var arrayOfPassPortArrays = prepareFile(file);
+    let passedPorts = [];
+    for (let x = 0; x < arrayOfPassPortArrays.length; x++) {
+        let thisPassport = arrayOfPassPortArrays[x];
+
+        let passNum = 0;
+        for (let j = 0; j < thisPassport.length; j++) {
+
+
+            let thisPassportCategory = thisPassport[j].split(':')[0];
+            let thisPassportValue = thisPassport[j].split(':')[1]
+
+            if (thisPassportCategory === 'byr') {
+                if (parseInt(thisPassportValue) >= 1920 && parseInt(thisPassportValue) <= 2020) {
+                    passNum = passNum + 1;
+                }
+            }
+            else if (thisPassportCategory === 'iyr') {
+                if (parseInt(thisPassportValue) >= 2010 && parseInt(thisPassportValue) <= 2020) {
+              
+                    passNum = passNum + 1;
+
+                }
+            }
+            else if (thisPassportCategory === 'eyr') {
+                if (parseInt(thisPassportValue) >= 2020 && parseInt(thisPassportValue) <= 2030) {
+                    passNum = passNum + 1;
+
+                }
+            }
+            else if (thisPassportCategory === 'hgt') {
+
+                let splitValue = thisPassportValue.split('');
+                let clippedCMorINCH = splitValue.splice(-2,).join('');
+
+                let height = thisPassportValue.slice(0, -2);
+
+
+                if (clippedCMorINCH === 'in') {
+                    if (height >= 59 && height <= 76) {
+                        passNum = passNum + 1;
+                    }
+                }
+                else if (clippedCMorINCH === 'cm') {
+                    if (height >= 150 && height <= 193) {
+                        passNum = passNum + 1;
+
+                    }
+                }
+            }
+            else if (thisPassportCategory === 'hcl') {
+                if (thisPassportValue.match(/^\#[a-z0-9]{6}/)) {
+                    passNum = passNum + 1;
+                }
+            }
+            else if (thisPassportCategory === 'ecl') {
+                if (thisPassportValue === 'amb' ||
+                    thisPassportValue === 'blu' ||
+                    thisPassportValue === 'brn' ||
+                    thisPassportValue === 'gry' ||
+                    thisPassportValue === 'grn' ||
+                    thisPassportValue === 'hzl' ||
+                    thisPassportValue === 'oth') {
+                    passNum = passNum + 1
+
+                }
+            }
+            else if (thisPassportCategory === 'pid') {
+                if (thisPassportValue.match(/^[0-9]{9}/)) {
+                    passNum = passNum + 1;
+                }
+            }
+            if (passNum === 7) {
+                passedPorts.push(thisPassport)
+            }
+
         }
-
     }
+    
+    console.log(passedPorts)
+    return passedPorts
+
+}
 
 
-console.log(validPassports('./input'))
+validPassports('./input')
